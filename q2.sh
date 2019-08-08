50eba51003f798f3234292618e27ce1e12d8294b
sandeepan-sahoo <53312092+sandeepan-sahoo@users.noreply.github.com>
2019-08-08 06:42:20 -0400

    Update q2.sh

--- q2.sh
+++ q2.sh
@@ -1,1 +1,11 @@
 
+#To accept a name and create folder and put 5 files of that name in it
+echo "Please enter your name"
+read name
+mkdir $name
+cd $name
+touch ${name}_1.cpp
+touch ${name}_2.cpp
+touch ${name}_3.cpp
+touch ${name}_4.cpp
+touch ${name}_5.cpp
